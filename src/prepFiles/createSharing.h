cd38e27c9a89e08e758eac8b1a36c879999bfdee
Unknown <pedromiguel.fernandes17@gmail.com>
2018-04-11 00:14:43 +0100

    added some documentation for prepFiles

--- src/prepFiles/createSharing.h
+++ src/prepFiles/createSharing.h
@@ -4,13 +4,48 @@
 #include <string>
 #include <set>
 
-#define MIN_LOT 5
-#define MAX_LOT 20
+/** @defgroup createsharing createsharing
+ * @{
+ *
+ * Various functions and constants to support the creation of sharing locations.
+ */
 
+#define MIN_LOT 5  /**< @brief Minimum lotation */
+#define MAX_LOT 20 /**< @brief Maximum lotation */
+
+/**
+ * @brief Create a Sharing File object
+ * 
+ * @param file 
+ * @param info 
+ */
 void createSharingFile(std::string file, const std::vector<std::string> &info);
 
+/**
+ * @brief 
+ * 
+ * @param min 
+ * @param max 
+ * @return int 
+ */
 int randBetween(int min, int max);
 
+/**
+ * @brief Create a Sharing Info object
+ * 
+ * @param ids 
+ * @param lineNumbers 
+ * @return std::vector<std::string> 
+ */
 std::vector<std::string> createSharingInfo(const std::vector<std::string> &ids, const std::set<int> &lineNumbers);
 
+/**
+ * @brief 
+ * 
+ * @param number 
+ * @param numLines 
+ * @return std::set<int> 
+ */
 std::set<int> generateRandomLineIDs(unsigned int number, int numLines);
+
+/** @} end of createsharing */
--- src/prepFiles/elevation.h
+++ src/prepFiles/elevation.h
@@ -3,14 +3,61 @@
 #include <vector>
 #include <string>
 
+/** @defgroup elevation elevation
+ * @{
+ *
+ * Various functions to support the request for elevation data and their results' parsing.
+ */
+
+/**
+ * @brief Get the Command object
+ * 
+ * @param locations 
+ * @return std::string 
+ */
 std::string getCommand(std::string locations);
 
+/**
+ * @brief 
+ * 
+ * @param file 
+ * @param what 
+ * @return std::vector<double> 
+ */
 std::vector<double> parseResult(std::string file, int what);
 
+/**
+ * @brief 
+ * 
+ * @param str 
+ * @return std::vector<std::string> 
+ */
 std::vector<std::string> split(std::string str);
 
+/**
+ * @brief Get the Elevations object
+ * 
+ * @param splited 
+ * @return std::vector<double> 
+ */
 std::vector<double> getElevations(const std::vector<std::string> &splited);
 
+/**
+ * @brief 
+ * 
+ * @param line 
+ * @return std::string 
+ */
 std::string parseLine(std::string line);
 
+/**
+ * @brief 
+ * 
+ * @param lines 
+ * @param begin 
+ * @param end 
+ * @return std::string 
+ */
 std::string parseLocations(const std::vector<std::string> &lines, int begin, int end);
+
+/** @} end of elevation */
